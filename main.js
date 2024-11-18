c38121313ffe95b74ef9ea04806a227dd2ebca4c
Andrew Perotti <Perotti28@gmail.com>
2024-11-18 11:38:07 -0800

    Change createDivisions and event listener funcs
    I had to change createDivisions function to accept not a count of total
    divisions but a count of column/row's instead, as the execercise told
    us to limit the user to 100 rows or columns, this was not implied
    earlier.
    I had to change the event listener logic to limit the input from the
    user, I also set a default value so if they only press enter then the
    original count of 16 columns/rows will be used.

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ let container = document.querySelector('#container')
 let changeButton = document.querySelector('button')
 let generatedDivs = document.querySelectorAll('#div[id^=div]')
 
-function createDivisions(divCount){
-    for(let i = 1; i<=divCount; i++){
+function createDivisions(rowColumnCount){
+    let divsCount = rowColumnCount*rowColumnCount
+    for(let i = 1; i<=divsCount; i++){
        let div = document.createElement("div")
        div.id = `div${i}`
        container.appendChild(div)
     }
 }
-createDivisions(256)
+createDivisions(16)
 
 container.addEventListener('mouseover', (e)=>{
     let target = e.target
@@ -19,7 +20,10 @@ container.addEventListener('mouseover', (e)=>{
 })
 
 changeButton.addEventListener('click', ()=>{
-    let userGridSize = Number(prompt("How many total squares would you like? "))
+    let userGridSize = Number(prompt("How many rows/columns of squares would you like to generate?(Maximum is 100) ", "16"))
+    if(userGridSize>100){
+        userGridSize=100
+    }
     while(container.firstChild){
         container.removeChild(container.firstChild)
     }
